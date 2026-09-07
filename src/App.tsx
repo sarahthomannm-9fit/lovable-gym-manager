1564c39cbf3429f2b11e156e7363dc432e09afa6
sarahthomannm-9fit <sarahthomannm@gmail.com>
2026-09-07 18:06:35 -0300

    feat: route segmented announcements

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import { RoleRoute } from "@/components/RoleRoute";
 import SelectContext from "./pages/SelectContext";
 import SindicoHome from "./pages/sindico/SindicoHome";
 import HealthDay from "./pages/sindico/HealthDay";
+import Announcements from "./pages/sindico/Announcements";
 import CoachHome from "./pages/coach/CoachHome";
 import ReviewQueue from "./pages/coach/ReviewQueue";
 import CorpHome from "./pages/corp/CorpHome";
@@ -182,6 +183,7 @@ const App = () => (
                     <Route path="/select-context" element={<ProtectedRoute><SelectContext /></ProtectedRoute>} />
                     <Route path="/sindico" element={<ProtectedRoute><RoleRoute allow={['sindico','admin']}><MainLayout><SindicoHome /></MainLayout></RoleRoute></ProtectedRoute>} />
                     <Route path="/sindico/health-day" element={<ProtectedRoute><RoleRoute allow={['sindico','admin']}><MainLayout><HealthDay /></MainLayout></RoleRoute></ProtectedRoute>} />
+                    <Route path="/sindico/comunicados" element={<ProtectedRoute><RoleRoute allow={['sindico','admin']}><MainLayout><Announcements /></MainLayout></RoleRoute></ProtectedRoute>} />
                     <Route path="/coach" element={<ProtectedRoute><RoleRoute allow={['professor','admin']}><MainLayout><CoachHome /></MainLayout></RoleRoute></ProtectedRoute>} />
                     <Route path="/coach/revisoes" element={<ProtectedRoute><RoleRoute allow={['professor','admin']}><MainLayout><ReviewQueue /></MainLayout></RoleRoute></ProtectedRoute>} />
                     <Route path="/corp" element={<ProtectedRoute><RoleRoute allow={['corporate','admin']}><CorpHome /></RoleRoute></ProtectedRoute>} />
